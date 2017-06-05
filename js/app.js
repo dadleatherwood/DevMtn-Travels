3700443dcc1cfcf9a46b488272cfa26ebbe0f226
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-06-05 13:16:30 -0500

    created states in app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,38 @@
 angular.module('devmtnTravel', ['ui.router'])
     .config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
+
             .state('home',{
                 url:'/',
                 templateUrl: "../views/about.html"
-            });
+            })
+
+            .state('packages',{
+              url: '/packages/:country',
+              templateUrl: 'views/packages.html'
+            })
+
+            .state('locations', {
+              url: '/locations',
+              templateUrl: 'views/locations.html'
+            })
+
+            .state('booked', {
+              url: '/booked/:id',
+              templateUrl: 'views/booked.html'
+            })
+
+            .state('adventurers', {
+              url: '/adventurers',
+              parent: '/',
+              templateUrl: 'views/about-adventurers.html'
+            })
+
+            .state('contact', {
+              url: '/contact',
+              parent: '/',
+              templateUrl: 'views/contact.html'
+            })
 
         $urlRouterProvider
             .otherwise('/');
